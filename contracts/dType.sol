536876818c018595908d9b0ee9fdc947b59197dd
Christian Tzurcanu <christian.tzurcanu@gmail.com>
2019-03-09 10:52:58 +0100

    Update dType.sol

--- contracts/dType.sol
+++ contracts/dType.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.5.1;
 contract dType{
     struct Type {
         string name;
-        string stype;
+        // string stype;
         bytes32[] types;
         address contractAddress;
+        bytes32 swarmId;
         uint256 index;
     }
     
@@ -19,51 +20,76 @@ contract dType{
         return (typeIndex[typeStruct[typeHash].index] == typeHash);
     }
     
-    function insert(string memory name, string memory stype, address contractAddress)        
+    function insert(string memory name, bytes32[] memory types, address contractAddress, bytes32 swarmId)        
     public returns(uint256 index){
-        bytes32 hash = keccak256(abi.encode(name, stype));
+        bytes32 hash = keccak256(abi.encode(name, types));
         if(isType(hash)) revert("This type exists. Use the extant type."); 
         typeStruct[hash].name = name;
-        typeStruct[hash].stype = stype;
+        typeStruct[hash].types = types;
         // typeStruct[hash].types = arrayOfEach(hash(type));
         typeStruct[hash].contractAddress = contractAddress;
+        typeStruct[hash].swarmId = swarmId;
         typeStruct[hash].index = typeIndex.push(hash)-1;
         
         emit LogNew(
             hash, 
             typeStruct[hash].index, 
             name, 
-            stype);
+            types);
         
         return typeIndex.length-1;
     }
     
+    function getTypes(bytes32 hash) public view
+    returns(bytes32[] memory types)
+    {
+        
+        if (typeStruct[hash].types.length == 1 && hash == typeStruct[hash].types[0]) return typeStruct[hash].types; 
+        bytes32[] memory ttypes;
+        for (uint256 i = 0 ; i< typeStruct[hash].types.length; i++) {
+            for (uint256 j =0 ; j< typeStruct[hash].types.length; j++) {
+                ttypes[ttypes.length] = getTypes(typeStruct[hash].types[i])[j];
+                //ttypes.length = ttypes.length + 1;
+            }
+        }
+        return ttypes;
+    }
+    
+    
+    function get(string memory aname, string memory astype) public view
+    returns(string memory name, bytes32[] memory types, uint256 index)
+    {
+        bytes32 hash = keccak256(abi.encode(aname, astype));
+        return getByHash(hash);
+        
+    }
+    
     
     function getByHash(bytes32 hash) public view
-    returns(string memory name, string memory stype, uint256 index)
+    returns(string memory name, bytes32[] memory types, uint256 index)
     {
         if(!isType(hash)) revert("No such type inserted."); 
         return(
             typeStruct[hash].name, 
-            typeStruct[hash].stype, 
+            typeStruct[hash].types, 
             typeStruct[hash].index);
     }
     
     // function getByName
     
     
-    function update(string memory name, string memory stype) 
+    function update(string memory name, bytes32[] memory types) 
     public returns(bool success) {
-        bytes32 hash = keccak256(abi.encode(name, stype));
+        bytes32 hash = keccak256(abi.encode(name, types));
         if(!isType(hash)) revert("No such type inserted.");
 
         typeStruct[hash].name = name;
-        typeStruct[hash].stype = stype;
+        typeStruct[hash].types = types;
         emit LogUpdate(
             hash,
             typeStruct[hash].index,
             name, 
-            typeStruct[hash].stype);
+            typeStruct[hash].types);
             
         return true;
     }
@@ -83,7 +109,7 @@ contract dType{
             keyToMove, 
             rowToDelete, 
             typeStruct[keyToMove].name, 
-            typeStruct[keyToMove].stype);
+            typeStruct[keyToMove].types);
         return rowToDelete;
     }
     
@@ -104,13 +130,13 @@ contract dType{
     bytes32 indexed typeHash, 
     uint256 index, 
     string name, 
-    string stype);
+    bytes32[] types);
     
     event LogUpdate(
     bytes32 indexed typeHash, 
     uint index, 
     string name, 
-    string stype);
+    bytes32[] types);
     
     event LogRemove(
     bytes32 indexed typeHash, 
