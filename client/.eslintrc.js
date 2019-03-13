3abfafcff56659ddda6dc613525e3d4ba333b235
Loredana <loredana.cirstea@gmail.com>
2019-03-13 19:19:33 +0200

    Init client app

---
+++ client/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'indent': ['error', 4],
+    'object-curly-spacing': ['error', 'never'],
+    'object-curly-newline': 'off',
+    'no-plusplus': 'off',
+    'func-names': ['error', 'never'],
+    'import/prefer-default-export': 'off',
+    'no-console': 'off',
+    "import/extensions": 'off',
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};
