21540ad30751fc5721d0ae0777f6526b2fbee028
Loredana <loredana.cirstea@gmail.com>
2019-03-21 00:48:33 +0200

    Temporary fix for Ganache

--- client/src/views/dTypeBrowse.vue
+++ client/src/views/dTypeBrowse.vue
@@ -32,19 +32,23 @@ export default {
             // const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:9545');
 
             // Metamask
-            // const provider = new ethers.providers.Web3Provider(web3.currentProvider);
-            // const wallet = new ethers.Wallet(DType.from.privateKey, provider);
+            let provider = new ethers.providers.Web3Provider(web3.currentProvider);
+            let wallet = new ethers.Wallet(DType.from.privateKey, provider);
 
-            // Ganache
-            const url = "http://localhost:8545";
-            const provider = new ethers.providers.JsonRpcProvider(url);
-            const wallet = provider.getSigner(0);
+            await waitAsync(1000);
+
+            // Temporary fix for Ganache
+            if (provider.network.chainId == 5777) {
+                const url = "http://localhost:8545";
+                provider = new ethers.providers.JsonRpcProvider(url);
+                wallet = provider.getSigner(0);
+                await waitAsync(1000);
+            }
 
             console.log(provider);
             console.log(wallet);
             console.log('DType', DType);
 
-            await waitAsync(1000);
             const contractAddress = DType.contract.networks[
                 String(provider.network.chainId)
             ].address;
