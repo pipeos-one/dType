a2f227237ac34c553e731b437c58eb084da95a4d
Loredana <loredana.cirstea@gmail.com>
2019-03-24 22:37:17 +0200

    Initialize vuex store actions in App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -12,5 +12,19 @@
 <script>
 export default {
     name: 'App',
+    created() {
+        this.setData();
+    },
+    destroyed() {
+        this.$store.dispatch('removeWatchers');
+    },
+    methods: {
+        async setData() {
+            await this.$store.dispatch('setProvider');
+            await this.$store.dispatch('setContract');
+            await this.$store.dispatch('setTypes');
+            this.$store.dispatch('watchAll');
+        },
+    },
 };
 </script>
