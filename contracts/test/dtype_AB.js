3266ea884263073641ae2af8af068490c7d9fc45
Loredana <loredana.cirstea@gmail.com>
2019-03-26 20:20:58 +0200

    Additional contract test for TypeB

--- contracts/test/dtype_AB.js
+++ contracts/test/dtype_AB.js
@@ -95,6 +95,22 @@ contract('dTypeAB', async (accounts) => {
         await dtype.setOutputs(hash, stakedFunction.outputs);
     });
 
+    it('insert data', async() => {
+        let logs, hash, index, typeBStruct;
+        let tokenAddress = '0xEAd8C52989b587B0c6a8478f8B6dd447E2fc8B1f';
+
+        ({logs} = await typeB.insert({
+            staked: 2,
+            typeA: {balance: 10, token: tokenAddress},
+        }));
+        ({hash, index} = logs[0].args);
+        typeBStruct = await typeB.getByHash(hash);
+
+        assert.equal(typeBStruct.staked, 2, 'run function incorrect output - staked');
+        assert.equal(typeBStruct.typeA.balance, 10, 'run function incorrect output - typeA.balance');
+        assert.equal(typeBStruct.typeA.token, tokenAddress, 'run function incorrect output - typeA.token');
+    });
+
     it('run AB function', async () => {
         let logs, hash, index;
         let tokenAddress = '0xEAd8C52989b587B0c6a8478f8B6dd447E2fc8B1f';
