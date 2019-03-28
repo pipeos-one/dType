5a3fd96e59f6993ee02c782491209e18262ff001
Loredana <loredana.cirstea@gmail.com>
2019-03-28 09:24:30 +0200

    Update Infura API to v3

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -69,7 +69,7 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
-        provider: () => new HDWalletProvider(privateKey, `https://ropsten.infura.io/${infuraKey}`),
+        provider: () => new HDWalletProvider(privateKey, `https://ropsten.infura.io/v3/${infuraKey}`),
         network_id: 3,       // Ropsten's id
         gas: 5500000,        // Ropsten has a lower block limit than mainnet
         confirmations: 2,    // # of confs to wait between deployments. (default: 0)
