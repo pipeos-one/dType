9daff0ccb1d34fe485645bb45f8500cac0c6d753
Loredana <loredana.cirstea@gmail.com>
2019-04-03 14:00:14 +0300

    Improve normalizeEthersObject

--- client/src/blockchain.js
+++ client/src/blockchain.js
@@ -107,23 +107,28 @@ export const buildDefaultItem = (dtype) => {
     return item;
 };
 
+export const isEthersObj = (item) => {
+    if (
+        item instanceof Array
+        && Object.keys(item).find(key => !Number(key) && Number(key) !== 0)
+    ) {
+        return true;
+    }
+    return false;
+};
+
 export const normalizeEthersObject = (item) => {
+    if (!(item instanceof Object)) return item;
+    if (item instanceof Object && item.toNumber) return item.toNumber();
+    if (item instanceof Array && !isEthersObj(item)) {
+        return item.map(value => normalizeEthersObject(value));
+    }
     let obj = {};
 
     Object.keys(item)
         .filter(key => !Number(key) && Number(key) !== 0)
         .forEach((key) => {
-            if (item[key] instanceof Object && item[key].toNumber) {
-                obj[key] = item[key].toNumber();
-            } else if (
-                item[key] instanceof Object &&
-                !(item[key] instanceof Array) &&
-                !item[key].toNumber
-            ) {
-                obj[key] = normalizeEthersObject(item[key]);
-            } else {
-                obj[key] = item[key];
-            }
+            obj[key] = normalizeEthersObject(item[key]);
         });
     return obj;
 };
