6cf1c5307290439882d6e3a83afc516ea3358bae
Loredana <loredana.cirstea@gmail.com>
2019-04-11 14:06:07 +0300

    Increase gas limit for testnet deployments
    Increase gas limit for testnet deployments from 5.5M to 6.5M
    dType deployment costs 6176575 gas

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -71,7 +71,7 @@ module.exports = {
     ropsten: {
         provider: () => new HDWalletProvider(privateKey, `https://ropsten.infura.io/v3/${infuraKey}`),
         network_id: 3,
-        gas: 5500000,
+        gas: 6500000,
         confirmations: 2,
         timeoutBlocks: 200,
         skipDryRun: true
@@ -80,7 +80,7 @@ module.exports = {
     kovan: {
         provider: () => new HDWalletProvider(privateKey, `https://kovan.infura.io/v3/${infuraKey}`),
         network_id:42,
-        gas: 5500000,
+        gas: 6500000,
         confirmations: 2,
         timeoutBlocks: 200,
         skipDryRun: true
@@ -89,7 +89,7 @@ module.exports = {
     rinkeby: {
         provider: () => new HDWalletProvider(privateKey, `https://rinkeby.infura.io/v3/${infuraKey}`),
         network_id: 4,
-        gas: 5500000,
+        gas: 6500000,
         confirmations: 2,
         timeoutBlocks: 200,
         skipDryRun: true
@@ -98,7 +98,7 @@ module.exports = {
     goerli: {
         provider: () => new HDWalletProvider(privateKey, `https://goerli.infura.io/v3/${infuraKey}`),
         network_id: 5,
-        gas: 5500000,
+        gas: 6500000,
         confirmations: 2,
         timeoutBlocks: 200,
         skipDryRun: true
