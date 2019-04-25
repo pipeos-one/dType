7ac2bb20aa610c1aaab13d8b2db6c36604a99f79
Loredana <loredana.cirstea@gmail.com>
2019-04-25 20:18:12 +0300

    Register voting types in dType

--- contracts/migrations/4_deploy_gov.js
+++ contracts/migrations/4_deploy_gov.js
@@ -3,6 +3,9 @@ const VoteResourceTypeLib = artifacts.require('VoteResourceTypeLib.sol');
 const VoteResourceTypeStorage = artifacts.require('VoteResourceTypeStorage.sol');
 const VotingFunctions = artifacts.require('VotingFunctions.sol');
 
+const dtypesGov = require('../data/dtypes_gov.json');
+const dtypesComposed = require('../data/dtypes_gov_composed.json');
+
 module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(VoteResourceTypeLib);
     await deployer.link(VoteResourceTypeLib, VoteResourceTypeStorage);
@@ -12,4 +15,20 @@ module.exports = async function(deployer, network, accounts) {
     let dtypeContract = await dType.deployed();
     let resourceStorage = await VoteResourceTypeStorage.deployed();
     let votingfunc = await VotingFunctions.deployed();
+
+    // Insert base types
+    dtypesGov.forEach(async (data) => {
+        switch(data.name) {
+            case "VoteResource":
+                data.contractAddress = resourceStorage.address;
+                break;
+        }
+        await dtypeContract.insert(data);
+    });
+
+    // Insert pure functions
+    dtypesComposed.forEach(async (data) => {
+        data.contractAddress = votingfunc.address;
+        await dtypeContract.insert(data);
+    });
 };
